f70605a27b469ebb6565fedf9b3daa18be292927
Pankaj Kumar <pk7zuva@users.noreply.github.com>
2021-10-23 12:39:45 -0400

    Create make-fpkm-matrix.sh

---
+++ make-fpkm-matrix.sh
@@ -0,0 +1,19 @@
+#############################################################
+##
+## Author: Pankaj Kumar
+## Email: pk7z@virginia.edu
+## Location: Bioinformatics Core
+##
+############################################################
+
+
+
+#Arg1 is the list of sample name "Sample_Name"
+#Arg2 output file name "rnaseq_workshop_fpkm_matrix.txt" 
+
+#usage bash /project/UVABX-PK/BIOINFORMATICS-CORE-RNASEQ-WORKSHOP-OCT-NOV-2021/script/make-fpkm-matrix.sh /scratch/$USER/rnaseq_workshop/Sample_Name /scratch/$USER/rnaseq_workshop/rnaseq_workshop_fpkm_matrix.txt
+cat $1 | awk '{printf ("%s\t",$1)}' | awk '{printf ("Ensembl-Gene-Id\tGene-Id\t%s\n",$0)}'> $2
+PASTE2=`cat $1 | awk '{printf ("%s/*gene_id-fpkm.out ",$1)}'`
+echo "paste $PASTE2" > Foo.sh
+
+sh ./Foo.sh | awk '{for (i=6; i<=NF; i=i+6) if(i==6) {printf ("%s\t%s\t%.1f\t",$1,$(i-3),$(i-1))} else if (i>6 && i<NF) {printf ("%.1f\t",$(i-1))}  else if (i==NF) {printf ("%.1f\n",$(i-1))}}' >> $2
